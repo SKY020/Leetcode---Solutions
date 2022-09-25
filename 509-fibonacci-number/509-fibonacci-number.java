03c7e1e43778ad9ffc2b3a9144da70f6f8b577b5
Shiv Kumar Yadav <shivamyadavsy020@gmail.com>
2022-09-25 17:56:43 +0530

    Time: 109 ms (5.57%), Space: 116.6 MB (5.96%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,20 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n<2){
-            return n;
-        }
-        return fib(n-1) + fib(n-2);
- }
-    
-    
-    // public int fib(int[] memo,int n){
-    //     if(memo[n]==0){
-    //         if(n<2){
-    //             memo[n]=n;
-    //         }
-    //         else{
-    //             int left = fin
-    //         }
-    //     }
-    // }
+//         if(n<2){
+//             return n;
+//         }
+//         return fib(n-1) + fib(n-2);
+        
+        if(n<=1) return n;
+        int min = -1;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,min);
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fib(n-1) + fib(n-2);
+    }
 }
